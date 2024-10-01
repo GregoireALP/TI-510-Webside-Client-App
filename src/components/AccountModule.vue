9b5812f5c070d84f3688fab910004bfa4aac3a39
GregoireALP <gregoire.alperovitch@gmail.com>
2024-10-01 17:23:11 +0200

    01/10/24 Class

---
+++ src/components/AccountModule.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h2>Bienvenue sur votre compte en banque</h2>
+
+    <div v-if="action === 'all'">
+        Toutes les actions
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccountModule',
+  props: ['action', 'id']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  font-family: sans-serif;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AccountModule from '@/components/AccountModule'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/account',
+      redirect: '/account/list/all'
+    },
+    {
+      path: '/account/:action/:id',
+      name: 'Account',
+      component: AccountModule,
+      props: true
     }
   ]
 })
