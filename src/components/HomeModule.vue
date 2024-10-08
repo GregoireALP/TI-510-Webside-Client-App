eca8df01a571cece668311163856008ff061d3e3
Nico9417 <nicolas.flandin@yahoo.com>
2024-10-08 17:06:12 +0200

    Update Home page

--- src/components/HomeModule.vue
+++ src/components/HomeModule.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        <h1>Hello World !</h1>
+        <h1> BABA BANK </h1>
+        <div class = "menu">
+            <div class = "client_link">
+                <a href = '/#/client/list/all'> Client </a>
+            </div>
+            <div class = "account_link">
+                <a href = '/#/account/list/all'> Account </a>
+            </div>
+            <div class = "payement_link">
+                <a href = '/#/payement/list/all'> Payement </a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,8 +22,37 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-    font-size: large;
-    font-family: sans-serif;
+.menu{
+    display: flex;
+    flex-wrap: wrap;
+    gap : 100px;
+    justify-content: center;
 }
+
+h1 {
+    text-decoration: underline;
+    color: rgb(15, 1, 1);
+    font-size: 2rem;
+    font-family: sans-serif;
+    text-align: center;
+    size: 20 px;
+    padding-bottom: 20px;
+    padding-top: 20px;
+}
+
+.client_link a{
+    color : rgb(19, 149, 160);
+    font-size: 1.3rem;
+}
+
+.account_link a{
+    color : rgb(19, 149, 160);
+    font-size: 1.3rem;
+}
+
+.payement_link a{
+    color : rgb(19, 149, 160);
+    font-size: 1.3rem;
+}
+
 </style>
