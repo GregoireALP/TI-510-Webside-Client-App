81e0adeb5a166c04d9c407d22f4f08fa05f3de0e
GregoireALP <gregoire.alperovitch@gmail.com>
2024-10-15 12:35:41 +0200

    Revert "WIP"

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/****************************************GLOBAL CSS*************************************/
+body {
+  padding: 0;
+  margin: 0;
+
+  background-color: rgb(227, 226, 226);
+}
+
+.title {
+  text-align: center;
+}
+</style>
