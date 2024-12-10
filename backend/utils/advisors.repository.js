ed1fbe08167bf216de8414f88bc40c229fcbd16c
GregoireALP <gregoire.alperovitch@gmail.com>
2024-12-10 15:24:00 +0100

    fhbsdlvc

--- backend/utils/advisors.repository.js
+++ backend/utils/advisors.repository.js
@@ -55,8 +55,8 @@ module.exports = {
 
         try {
 
-            let sql = "INSERT INTO advisor (advisor_firstname, advisor_gender, advisor_lastname, advisor_email, advisor_phone, advisor_address, advisor_birthday, advisor_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-            const [rows, fields] = await pool.query(sql, ['Advisor', 1, 'Bank', 'magnis.dis@yahoo.com', '0000000000', '1 Bank Street', '2021-01-01', 'test']);
+            let sql = "INSERT INTO advisor (advisor_firstname, advisor_gender, advisor_lastname, advisor_email, advisor_phone, advisor_address, advisor_birthday, advisor_password) VALUES (?, ?, ?, ?, ?, ?, now(), ?)";
+            const [rows, fields] = await pool.query(sql, ['Advisor', 1, 'Bank', 'magnis.dis@yahoo.com', '0000000000', '1 Bank Street', 'test']);
             return 'Ok';
         } catch (error) {
             console.log(error);
